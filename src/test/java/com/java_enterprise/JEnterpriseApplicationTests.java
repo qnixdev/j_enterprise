7938dcd2eed11a638a33d7c0da81f52104094ccb
qnixdev <qnixdev@gmail.com>
2024-03-17 14:08:08 +0200

    init maven

---
+++ src/test/java/com/java_enterprise/JEnterpriseApplicationTests.java
@@ -0,0 +1,10 @@
+package com.java_enterprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JEnterpriseApplicationTests {
+    @Test
+    void contextLoads() {}
+}
